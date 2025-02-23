5738c11f52dd6f0c3321a6f50090b5d4fb146d35
Martin Kunz <martinkunz@email.cz>
2025-02-23 13:09:48 +0100

    add: Initial implementation of the algorithm

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,13 @@ using Aqua
 
 @testset "RandomizedSparsification.jl" begin
     @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(RandomizedSparsification)
+        Aqua.test_all(RandomizedSparsification; piracies=(; broken=true))
     end
-    # Write your tests here.
+    @testset "rank" begin
+        using LinearAlgebra
+        using RandomizedSparsification: srank, rankrand
+        A_large = rankrand(1000, 1500, 20)
+        @test rank(A_large) == 20
+        @test srank(A_large) <= rank(A_large)
+    end
 end
