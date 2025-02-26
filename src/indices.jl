0426f4230005dbe8d6260adf49ab93dcfd08a067
Martin Kunz <martinkunz@email.cz>
2025-02-26 15:03:45 +0100

    fix(docs): FindIndices module documentation `_indices` signature

--- src/indices.jl
+++ src/indices.jl
@@ -1,28 +1,26 @@
 """
-This module contains functions for determining the indices from the cumulative sum of the probability matrix and the
-randomly sampled numbers from ``[0,1]`` of the length of the desired number of samples.
+This module contains functions for determining the indices from the cumulative sum of the probability matrix `P_ij_cs`
+(i.e. cumulative distribution function on the indices) and the randomly sampled numbers from ``[0,1]`` of the length of
+the desired number perspired by the user to align with the necessary accuracy bounds.
 
-It does not consider whether or not the number of the samples that are taken fit into the memory or not. This should be
-considered elsewhere in the call stack where the number of samples desired can be separated into batches that should be
-sampled at once and only the matrix should be held between the iterations.
+!!! danger
+    The methods do not check whether or not the number of the samples that are taken fit into the RAM memory. This
+    should be considered before in the call stack. The number of samples desired must be separated into batches that fit
+    into the memory of the particular algorithm used for the indices location.
 
-The functions that find the summation indices in the sparsification algorithm have the signature signature: 
+The functions that find the summation indices in the sparsification algorithm have the signature: 
 ```julia
 _indices(
-    P_ij_cs::AbstractVector{<:Real}, 
-    rand01_vec::AbstractVector{<:Real},
-    m::Integer, n::Integer
-)::AbstractDict{<:CartesianIndex, <:Unsigned}
+    P_ij_cs::AbstractVector{<:Real},    # cumulative distribution function on matrix indices
+    rand01_vec::AbstractVector{<:Real}, # sampled values in [0,1]
+    m::Integer, n::Integer              # original matrix size
+)::AbstractVector{CartesianIndex{2}}
 ```
-where `P_ij_cs` is the vector of the cumulative sum of the probabilities, `rand01_vec` are the sampled values and `m`
-and `n` are the dimensions of the original matrix. So the indices are returned in the form of
-a `<:AbstractDict{<:CartesianIndex, <:Unsigned}`, where the `Unsigned` holds the counts of the samples of
-`ind::CartesianIndex`.
-""" 
+So the indices are returned as the vector `[ij::CartesianIndex{2}...]` with repetitions. This is passed to the
+[`sparse`](@extref SparseArrays.sparse) function for the creation of the sparsified matrix.
+"""
 module FindIndices
-@inline function _indices_sequencial()
-end
+@inline function _indices_sequencial() end
 
-@inline function _indices_pivoting()
-end
+@inline function _indices_pivoting() end
 end
