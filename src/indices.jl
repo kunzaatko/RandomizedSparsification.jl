63aff438a70710f14db61c1855369d180c3e6436
Martin Kunz <martinkunz@email.cz>
2025-02-26 17:13:37 +0100

    feat: two methods to find indices
    1. sequential
    2. group sort which employs the DataFrames sort method

--- src/indices.jl
+++ src/indices.jl
@@ -3,24 +3,59 @@ This module contains functions for determining the indices from the cumulative s
 (i.e. cumulative distribution function on the indices) and the randomly sampled numbers from ``[0,1]`` of the length of
 the desired number perspired by the user to align with the necessary accuracy bounds.
 
+
 !!! danger
-    The methods do not check whether or not the number of the samples that are taken fit into the RAM memory. This
-    should be considered before in the call stack. The number of samples desired must be separated into batches that fit
-    into the memory of the particular algorithm used for the indices location.
+    1. The methods do not check whether or not the number of the samples that are taken fit into the RAM memory.
+        This should be considered before in the call stack. The number of samples desired must be separated into
+        batches that fit into the memory of the particular algorithm used for the indices location.
+    2. Assumption that `P_ij_cs` fits into memory is made. If it does not we need to be able to compute it at evaluation
+        time for a given index efficiently (which is [not yet
+        implemented](https://github.com/kunzaatko/RandomizedSparsification.jl/issues/1)).
 
 The functions that find the summation indices in the sparsification algorithm have the signature: 
 ```julia
 _indices(
     P_ij_cs::AbstractVector{<:Real},    # cumulative distribution function on matrix indices
-    rand01_vec::AbstractVector{<:Real}, # sampled values in [0,1]
-    m::Integer, n::Integer              # original matrix size
+    rands01::AbstractVector{<:Real},    # sampled values in [0,1]
+    m::Integer, n::Integer;             # original matrix size
+    progress=false                      # report progress to the user
 )::AbstractVector{CartesianIndex{2}}
 ```
 So the indices are returned as the vector `[ij::CartesianIndex{2}...]` with repetitions. This is passed to the
 [`sparse`](@extref SparseArrays.sparse) function for the creation of the sparsified matrix.
 """
 module FindIndices
-@inline function _indices_sequencial() end
+using DataFrames
+@inline function _indices_sequencial(P_ij_cs, rands01, m, n; progress=false)
+    # FIX: suppresses all the logging. Instead I would like to suppress only the progress logging. Is there a way of
+    # doing that using `ProgressLogging.jl`? <25-02-25> 
+    # Logging.with_logger(progress ? Logging.current_logger() : Logging.NullLogger()) do
+    # @progress for c in 
+    return map(p -> CartesianIndices((m, n))[searchsortedfirst(P_ij_cs, p)], rands01)
+end
 
-@inline function _indices_pivoting() end
+"""
+    _indices_groupsort(P_ij_cs, rands01, m, n)
+    
+# Algorithm
+1. Defines a [`DataFrame`](@extref DataFrames.DataFrame) holiding `P_ij_cs` and `rands01` in the `:vals` column having a boolean `:group` column that differentiates values from `P_ij_cs` from `rands01`.
+2. Rows are sorted by the `:vals` columns interleaving the `rands01`.
+3. Indices are determined by mapping the number of preceding "`:group == P_ij_cs`" values for ta given `:group == rands01`
+
+!!! perf 
+        `DataFrames.jl` should only bring forward a small overhead compared to doing the underlying sorting manually.
+"""
+@inline function _indices_groupsort(P_ij_cs, rands01, m, n; progress=false) 
+    df = DataFrame(:vals=>[P_ij_cs; rands01], :group=>[fill(true, length(P_ij_cs)); fill(false, length(rands01))])
+    # TODO: Choose algorithm for sorting that uses the fact that the vector of P_ij_cs is already sorted. <26-02-25> 
+    sort!(df, [:vals, :group])
+    df[!, :ind] = cumsum(df[!,  :group])
+    return getindex(CartesianIndices((m,n)), df[df[!, :group] .== false ,:ind] .+ 1)
+end
+
+# TODO: Recursive function that gives the index when it finds it otherwise it splits the input into two and passes the
+# splits to the function once it otherwise it splits the input into two and passes the splits to the function once again <26-02-25> 
+@inline function _indices_pivoting(P_ij_cs, rands01, m, n; progress=false) end
+
+# TODO: Two indexed piping indexes algorithm <26-02-25> 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -13,4 +13,26 @@ using Aqua
         @test rank(A_large) == 20
         @test srank(A_large) <= rank(A_large)
     end
+    @testset "FindIndices" begin
+        using RandomizedSparsification: FindIndices as FI
+
+        P_ij = [1 2 3; 4 5 6; 7 8 9] / sum(1:9)
+        P_ij_cs = cumsum(view(P_ij, :))
+        rand01_vec = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        out = [
+            CartesianIndex(1, 1), # 0.01 <= 0.0222222
+            CartesianIndex(2, 1), # 0.0222222 < 0.1 <= 0.1111111 
+            CartesianIndex(3, 1), # 0.1111111 < 0.2 <= 0.266667  
+            CartesianIndex(1, 2), # 0.266667 < 0.3 <= 0.311111
+            CartesianIndex(2, 2), # 0.311111 < 0.4 <= 0.422222 
+            CartesianIndex(3, 2), # 0.422222 < 0.5 <= 0.6
+            CartesianIndex(3, 2), # 0.422222 < 0.6 <= 0.6
+            CartesianIndex(2, 3), # 0.666667 < 0.7 <= 0.8
+            CartesianIndex(2, 3), # 0.666667 < 0.8 <= 0.8
+            CartesianIndex(3, 3), # 0.8 < 0.9
+        ]
+
+        @test FI._indices_sequencial(P_ij_cs, rand01_vec, 3, 3) == out
+        @test FI._indices_groupsort(P_ij_cs, rand01_vec, 3, 3) == out
+    end
 end
